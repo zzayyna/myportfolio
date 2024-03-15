e30b56128ddd9102f32b9889b7b6e9e01283ea17
zzayyna <144557938+zzayyna@users.noreply.github.com>
2024-03-15 18:07:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+/*REVEAL ON SCROLL */
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+  var revealSide = document.querySelectorAll('.revealSide');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+    else {
+      reveals[i].classList.remove('active');
+    }
+  }
+
+  for (var i = 0; i < revealSide.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = revealSide[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      revealSide[i].classList.add('active');
+    }
+    else {
+      revealSide[i].classList.remove('active');
+    }
+  }
+}
+
+/*MENU*/
+var menuButton = $('nav');
+var menu = $('.menu');
+var everything = $('.everything')
+menuButton.on('click', showMenu);
+
+function showMenu() {
+  menu.toggleClass('showMenu');
+  everything.toggleClass('darken');
+} 
